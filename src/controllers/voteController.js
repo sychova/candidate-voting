7618bc85daa359acfa71454418b53211b693934d
sychova <anastasiya.sychova@gmail.com>
2022-05-14 03:08:23 +0300

    country test

--- src/controllers/voteController.js
+++ src/controllers/voteController.js
@@ -1,9 +1,13 @@
+const { lookup } = require('geoip-lite')
 const { voteSubmitter } = require('../services/index')
 
 const submitVote = async (req, res) => {
   try {
-    await voteSubmitter.call(req.body)
-    res.render('./index')
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    console.log(ip) // ip address of the user
+    console.log(lookup(ip)) // location of the user
+    // await voteSubmitter.call(req.body)
+    // res.render('./index')
   } catch (error) {
     res.status(500).send(error)
   }
