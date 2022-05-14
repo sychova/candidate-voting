1d12e529268b438867c2e86acdb3ece506494236
sychova <anastasiya.sychova@gmail.com>
2022-05-14 03:55:59 +0300

    country test

--- src/controllers/voteController.js
+++ src/controllers/voteController.js
@@ -4,7 +4,7 @@ const { voteSubmitter } = require('../services/index')
 const submitVote = async (req, res) => {
   try {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-    if (lookup(ip).country === 'BY') {
+    if (lookup(ip).country !== 'BY') {
       await req.flash('error', 'Sorry, voting only allowed from Belarus.')
     } else {
       await voteSubmitter.call(req.body)
